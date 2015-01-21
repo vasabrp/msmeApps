52b1eeb05fa0d3d13da10c2867232b56169855c3
vasabrp <vasa.buraphadeja@gmail.com>
2015-01-21 22:55:24 +0700

    project-report.php

--- project-report.php
+++ project-report.php
@@ -1,27 +1,35 @@
 <?php
-
+$login = 1;
 session_start();
-include 'project-report.html.php';
+$_SESSION["user_id"] = "440262";
 
-// https://github.com/google/google-api-php-client
-// http://stackoverflow.com/questions/26771936/cant-find-google-client-php-in-google-api-php-client-library
-require_once $_SERVER['DOCUMENT_ROOT'].'/google-api/autoload.php'; 
+if(isset($_SESSION["user_id"])) {
 
-$scriptUri = "http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
-$client = new Google_Client();
-$client->setAccessType('online'); // default: offline
-$client->setApplicationName('kitkat-pandanus');
-$client->setClientId('367641634400-9qht8btr0vcc1ki7k3u6qchijgejetsh.apps.googleusercontent.com');
-$client->setClientSecret('Dr_Ese0nEoHVXJN8DQ2oyfQk');
-$client->setRedirectUri($scriptUri);
-$client->setDeveloperKey('AIzaSyDjboLstA0zkBPViSgb4xOg_a3LdUzyNdc'); // API key
+	include 'project-report.html.php';
 
-$service = new Google_Service_Books($client);
-$optParams = array('filter' => 'free-ebooks');
-$results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
+	// https://github.com/google/google-api-php-client
+	// http://stackoverflow.com/questions/26771936/cant-find-google-client-php-in-google-api-php-client-library
+	require_once $_SERVER['DOCUMENT_ROOT'].'/google-api/autoload.php'; 
 
-foreach ($results as $item) {
-  echo $item['volumeInfo']['title'], "<br /> \n";
+	$scriptUri = "http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
+	$client = new Google_Client();
+	$client->setAccessType('online'); // default: offline
+	$client->setApplicationName('kitkat-pandanus');
+	$client->setClientId('367641634400-9qht8btr0vcc1ki7k3u6qchijgejetsh.apps.googleusercontent.com');
+	$client->setClientSecret('Dr_Ese0nEoHVXJN8DQ2oyfQk');
+	$client->setRedirectUri($scriptUri);
+	$client->setDeveloperKey('AIzaSyDjboLstA0zkBPViSgb4xOg_a3LdUzyNdc'); // API key
+
+	$service = new Google_Service_Books($client);
+	$optParams = array('filter' => 'free-ebooks');
+	$results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
+
+	foreach ($results as $item) {
+		echo $item['volumeInfo']['title'], "<br /> \n";
+	}
+
+} else {
+	
 }
 
 echo 'Hello, world.';
